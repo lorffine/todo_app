d8885225ad2f96025e9cfa36fad98c0ecba97c12
fsigo <eljahmed40@gmail.com>
2023-11-19 00:32:41 +0100

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoListCompComponent } from './todo-list-comp/todo-list-comp.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListCompComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
